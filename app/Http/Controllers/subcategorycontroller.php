e33a4dfcca3507ca2e9cdbdd7038616afd1ad486
Kishan Baraiya <kishanbaraiya2002@gmail.com>
2024-01-19 14:14:50 +0530

    added

--- app/Http/Controllers/subcategorycontroller.php
+++ app/Http/Controllers/subcategorycontroller.php
@@ -10,17 +10,17 @@ use Illuminate\Support\Str;
 class subcategorycontroller extends Controller
 {
   
-    public function Addsubcategory(){
-        $categories = category::orderBy('category_name','ASC')->get();
-        return view('category.index',compact('categories'));
-     }
-    public function storesubcategory(Request $request){
+    // public function Addsubcategory(){
+    //     $categories = category::orderBy('category_name','ASC')->get();
+    //     return view('category.index',compact('categories'));
+    //  }
+    // public function storesubcategory(Request $request){
 
-        $subcategory = new subcategory;
-        $subcategory->category_id = $request->category_id;
-        $subcategory->subcategory_name =$request->subcategory_name;
-        $subcategory->subcategory_slug =str::slug( $request->subcategory_slug);
-        $subcategory->save();
-        return back();
-    }
+    //     $subcategory = new subcategory;
+    //     $subcategory->category_id = $request->category_id;
+    //     $subcategory->subcategory_name =$request->subcategory_name;
+    //     $subcategory->subcategory_slug =str::slug( $request->subcategory_slug);
+    //     $subcategory->save();
+    //     return back();
+    // }
 }
